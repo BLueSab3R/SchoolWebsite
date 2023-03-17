ecee709a26d4c137f6451a1f8125860ba917a575
BLueSab3R <100284691+BLueSab3R@users.noreply.github.com>
2023-03-17 22:14:24 +0200

    refactoring

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,30 +4,27 @@ import { Link } from 'react-router-dom'
 function Header() {
     return (
         <div className={style.header}>
-            <div className={style.title}>
-                <img width={70} height={70} src='/images/logo-removebg-preview.png' alt='log' />
-                <h1>
-                    САЙТ ШПИТЬКІВСЬКОЇ ЗАГАЛЬНООСВІТНЬОЇ ШКОЛИ І-ІІІ СТУПЕНІВ
-                    КИЇВСЬКОЇ ОБЛАСТІ БУЧАНСЬКОГО РАЙОНУ ДМИТРІВСЬКОЇ СІЛЬСЬКОЇ РАДИ
+            <div className={style.logo}>
+                <img className={style.photo} width={80} height={90} src='/images/logo-removebg-preview.png' alt='log' />
+                <h1 className={style.text}>
+                    Сайт школи 'Скіф'
                 </h1>
             </div>
-            <div className={style.fastMenu}>
-                <ul>
-                    <li>
-                        <div class="dropdown">
-                            <Link to='/'> <button class="dropbtn">Головна</button> </Link>
-                            <div class="dropdown-content">
-                                <Link to='schoolHistory'>Історія школи</Link>
-                                <Link to='pedagogichiyColectiv'>Педагогічний колектив</Link>
-                                <Link to='lyceumTarget'>Місія ліцею</Link>
-                                <Link to='schoolGoal'>Мета</Link>
-                                <Link to='visionary'>Візія</Link>
-                                <Link to='traditions'>Наші традиції</Link>
-                            </div>
+            <ul>
+                <li>
+                    <div class="dropdown">
+                        <Link to='/'> <button class="dropbtn">Головна</button> </Link>
+                        <div class="dropdown-content">
+                            <Link to='schoolHistory'>Історія школи</Link>
+                            <Link to='pedagogichiyColectiv'>Педагогічний колектив</Link>
+                            <Link to='lyceumTarget'>Місія ліцею</Link>
+                            <Link to='schoolGoal'>Мета</Link>
+                            <Link to='visionary'>Візія</Link>
+                            <Link to='traditions'>Наші традиції</Link>
                         </div>
-                    </li>
-                </ul>
-            </div>
+                    </div>
+                </li>
+            </ul>
         </div >
     )
 }
