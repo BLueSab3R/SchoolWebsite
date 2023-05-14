5dd88ac65a84238edab8fad2fd2ad7d6fe23a252
BLueSab3R <100284691+BLueSab3R@users.noreply.github.com>
2023-05-15 01:32:01 +0300

    фів

--- src/information/Information.jsx
+++ src/information/Information.jsx
@@ -1,24 +1,35 @@
+//  import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 import style from './Information.module.scss'
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 function Information() {
     return (
         <div className={style.wrapper}>
-            <div className={style.block}>
-                <div className={style.links}>
-                    <p>Корисні посилання</p>
-                    <a className={style.link} href='www.mon.gov.ua/'>Міністерство освіти і науки</a>
-                    <a className={style.link} href='http://testportal.gov.ua/'>Український центр оцінювання якості освіти</a>
-                    <a className={style.link} href='https://lms.e-school.net.ua/'>Всеукраїнська школа онлайн</a>
-                    <a className={style.link} href='http://kristti.com.ua/'>Київський обласний інститут підвищення освіти педагогічних кадрів</a>
-                </div>
+            <div className={style.links}>
+                <p>Корисні посилання</p>
+                <a className={style.link} href='www.mon.gov.ua/'>Міністерство освіти і науки</a>
+                <a className={style.link} href='http://testportal.gov.ua/'>Український центр оцінювання якості освіти</a>
+                <a className={style.link} href='https://lms.e-school.net.ua/'>Всеукраїнська школа онлайн</a>
+                <a className={style.link} href='http://kristti.com.ua/'>Київський обласний інститут підвищення освіти педагогічних кадрів</a>
             </div>
             <div className={style.content}>
-                <Link className={style.links} to='content'><h4>Зміст сайту</h4></Link>
+                <Link className={style.links} to='content'><h4>ЗМІСТ САЙТУ</h4></Link>
             </div>
-            <h4 className={style.number}>
+            <div className={style.social}>
+                <a href='https://www.facebook.com/profile.php?id=100075877736036'
+                    className='youtube social'
+                >
+                    <FontAwesomeIcon icon={faFacebook}
+                        size='3x'
+                    />
+                </a>
+            </div>
+            {/* <h4 className={style.number}>
                 Телефон: 0459876593
-            </h4>
+            </h4> */}
         </div >
 
 
