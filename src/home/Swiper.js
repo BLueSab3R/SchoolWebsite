4914588e6ee76d11245638433046f4816d4a6b83
BLueSab3R <100284691+BLueSab3R@users.noreply.github.com>
2023-05-19 14:34:36 +0300

    фвфі

--- src/home/Swiper.js
+++ src/home/Swiper.js
@@ -1,15 +1,14 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Autoplay, Pagination } from "swiper";
+import SwiperCore, { Autoplay, EffectFade, Pagination } from "swiper";
 import style from './Home.module.scss'
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 
-SwiperCore.use([Autoplay, Pagination]);
-
-// import required modules
+SwiperCore.use([Autoplay, EffectFade, Pagination]);
 
 export default function App() {
   const slides = [
@@ -24,55 +23,57 @@ export default function App() {
     { image: '/slider/9.jpg' },
     { image: '/slider/10.jpg' },
   ];
+
   const [swiper, setSwiper] = useState(null);
   const [activeIndex, setActiveIndex] = useState(0);
-
+  const [showInformation, setShowInformation] = useState(true);
 
   const handleSlideChange = () => {
     if (swiper) {
       setActiveIndex(swiper.realIndex);
       if (swiper.isEnd) {
         setTimeout(() => {
-          swiper.slideTo(0, 500); // Змініть швидкість переходу за необхідності
+          swiper.slideTo(0, 500);
         }, 500);
       }
+      setShowInformation(false);
+      setTimeout(() => {
+        setShowInformation(true);
+      }, 300);
     }
-  }
-
+  };
 
   return (
     <div className={style.container}>
-      <>
-        <Swiper
-          loop={false}
-          freeMode={false}
-
-          onSwiper={setSwiper}
-          onSlideChange={handleSlideChange}
-          spaceBetween={0}
-          slidesPerView={1}
-          effect='slide'
-          // centeredSlides={true}
-          autoplay={{
-            delay: 8000,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-            renderBullet: function (index, className) {
-              return `<span class="${className}"></span>`;
-            },
-          }}
-          className={style.mySwiper}
-        >
-          {slides.map((slide, index) => (
-            <SwiperSlide key={index}>
-              <img src={slide.image} alt={`Slide${index + 1}`}></img>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </>
+      <Swiper
+        loop={false}
+        freeMode={false}
+        onSwiper={setSwiper}
+        onSlideChange={handleSlideChange}
+        spaceBetween={0}
+        slidesPerView={1}
+        effect='fade'
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+          renderBullet: function (index, className) {
+            return `<span class="${className}"></span>`;
+          },
+        }}
+        className={style.mySwiper}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <img src={slide.image} alt={`Slide${index + 1}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className={`${style.information} ${showInformation ? style.show : style.hide}`}>
+        {/* Content of the information element */}
+      </div>
     </div>
-
   );
 }
